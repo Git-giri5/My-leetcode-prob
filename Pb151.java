047e84eff4f58895a373b3f04b411a5357fdbc3d
Git-giri5 <program102015@gmail.com>
2026-07-14 10:41:37 +0530

    Modifying pb 151

--- Pb151.java
+++ Pb151.java
@@ -1,3 +1,4 @@
+//this is 12ms
 class Solution {
     public String reverseWords(String s) {
         String[] arr=new String(s).split(" ");
@@ -13,3 +14,20 @@ class Solution {
         return String.join(" ", arr).trim().replaceAll("\\s+"," ");
     }
 }
+/*  
+less time complexity than above this is 8ms
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr=s.trim().split("\\s+");
+        int j=arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            if(i<j){
+                String temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j--;
+            }
+        }
+        return String.join(" ", arr);
+    }
+} */
