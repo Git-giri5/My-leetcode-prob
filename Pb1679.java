0ca0c4f95af44392692389c78170ca596e1e06d7
Git-giri5 <program102015@gmail.com>
2026-07-21 15:20:21 +0530

    Adding pb 1679 adding my concep

--- Pb1679.java
+++ Pb1679.java
@@ -15,7 +15,17 @@ class Solution {
                 r--;
             }
         }
-        
+        /*for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==k){
+                    c+=1;
+                    nums[i]=0;
+                    nums[j]=0;
+                }else{
+                    continue;
+                }
+            }
+        }*/
         
         return c;
     }
