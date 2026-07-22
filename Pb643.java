48b3f6bbf84cd17cfe5190aee6abe64c7f98747f
Git-giri5 <program102015@gmail.com>
2026-07-22 19:51:45 +0530

    Adding pb 643 new code

--- Pb643.java
+++ Pb643.java
@@ -1,4 +1,5 @@
-/*class Solution {
+/* My code time limit exist
+class Solution {
     public double findMaxAverage(int[] nums, int k) {
         double m=Double.NEGATIVE_INFINITY;
         for(int i=0;i<=nums.length-k;i++){
@@ -11,3 +12,24 @@
         return m/k;
     }
 }*/
+// Ai code
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+            sum = sum + nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return (double) maxSum / k;
+    }
+}
