a3e9df037bf2c498886fd9726fbdb3ec1a95e639
Git-giri5 <program102015@gmail.com>
2026-07-25 19:51:29 +0530

    Adding pb1493

---
+++ Pb1493.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int l=0,m=0,zeroco=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zeroco++;
+            }
+            while(zeroco>1){
+                if(nums[l]==0){
+                    zeroco--;  
+                }
+                l++;
+            }
+            m=Math.max(m,i-l);
+        }
+        return m;
+    }
+}
