b82f3f5a40012b229189f75df0dc7543d095cad9
Git-giri5 <program102015@gmail.com>
2026-07-26 11:51:22 +0530

    Adding pb1732

---
+++ Pb1732.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] arr=new int[gain.length+1];
+        arr[0]=0;
+        int m=0;
+        for(int i=1;i<=gain.length;i++){
+            arr[i]=gain[i-1]+arr[i-1];
+            m=Math.max(m,arr[i]);
+            
+        }
+        return m;
+    }
+}
