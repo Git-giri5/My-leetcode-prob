999b9a48b38de0299b000928c3640be3ab04e6c3
Giri <program102015@gmail.com>
2026-09-20 14:52:14 +0530

    Implement dailyTemperatures method in Solution class

---
+++ Pb739.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int i = 0; i < n; i++) {
+
+            while (top >= 0 &&
+                   temperatures[stack[top]] < temperatures[i]) {
+
+                int j = stack[top--];
+                ans[j] = i - j;
+            }
+
+            stack[++top] = i;
+        }
+
+        return ans;
+    }
+}
