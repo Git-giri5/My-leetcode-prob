738ea6307b566ac1350c2fbe593f2b094f55cabf
Giri <program102015@gmail.com>
2026-09-25 21:55:24 +0530

    Update Pb1679.java

--- Pb1679.java
+++ Pb1679.java
@@ -15,7 +15,8 @@ class Solution {
                 r--;
             }
         }
-        /*for(int i=0;i<nums.length;i++){
+        /*2nd cod3
+        for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==k){
                     c+=1;
